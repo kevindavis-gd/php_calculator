87c447a6ef862840fec7c9dd2aa4794c37e78d06
kevindavis-gd <kevindavis.gd@gmail.com>
2019-06-11 22:22:24 -0500

    first

---
+++ php_calculator.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+
+<form>
+	<input type="text" name="num1" placeholder="number 1">
+	<input type="text" name="num2" placeholder="number 2">
+	<select name ="operator">
+		<option>None</option>
+		<option>Add</option>
+		<option>Subtract</option>
+		<option>Multiply</option>
+		<option>Divide</option>
+	</select>
+	<br>
+	<button name ="submit" type ="submit" value= "submit">Calculate</button>
+
+</form>
+
+
+
+</body>
+</html>
