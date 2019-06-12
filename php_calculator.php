455468e89aa6fc86004fb489a76985fa0ad55e40
kevindavis-gd <kevindavis.gd@gmail.com>
2019-06-11 23:37:27 -0500

    commit

--- php_calculator.php
+++ php_calculator.php
@@ -18,8 +18,39 @@
 	</select>
 	<br>
 	<button name ="submit" type ="submit" value= "submit">Calculate</button>
-
 </form>
+
+
+<?php
+
+	if (isset($_GET['submit']))
+	{
+		$result1 = $_GET["num1"];
+		$result2 = $_GET["num2"];
+		$operator = $_GET["operator"];
+		echo 'Answer = ';
+	
+		switch($operator)
+		{
+			case "None":
+				echo "Please Select Operator";
+				break;
+			case "Add":
+				echo $result1 + $result2;
+				break;
+			case "Subtract":
+				echo $result1 - $result2;
+				break;
+			case "Multiply":
+				echo $result1 * $result2;
+				break;
+			case "Divide":
+				echo $result1 / $result2;
+				break;
+		}
+	}
+?>
+
 
 
 
